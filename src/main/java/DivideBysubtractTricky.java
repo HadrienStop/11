f9f8fba75880047657e3a0c8a4b345c7ab1fecea
s20025226 <hadrienlma@gmail.com>
2021-09-28 13:26:57 +0200

    implementation of DivideBysubtractTricky

---
+++ src/main/java/DivideBysubtractTricky.java
@@ -0,0 +1,20 @@
+public class DivideBysubtractTricky implements DivideAlgorithm{
+
+    private int operationCount;
+    private int result;
+    private int multiple;
+
+    @Override
+    public int run(int numerator, int denominator) {
+        result = 0;
+        operationCount = 0;
+        multiple = 1;
+        while (numerator >= denominator){
+            numerator = numerator - multiple * denominator;
+            result++;
+            multiple++;
+            operationCount++;
+        }
+        return result;
+    }
+}
